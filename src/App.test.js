c208d1bb33e6e6699c110cdbe98982642a9cb36a
Nicola Küng <32466285+nkueng@users.noreply.github.com>
2023-08-05 18:17:26 +0000

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders "Bremgarten" name', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Bremgarten/i);
-  expect(linkElement).toBeInTheDocument();
-});
