276df2e77f6f4357b4db3fd0ce926d5ec9eeb393
Nicola Küng <32466285+nkueng@users.noreply.github.com>
2023-08-31 22:38:29 +0200

    Adding functionality to render measurement data and adding bungee locations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
 import "./App.css";
+// import { river_locations, bungee_locations } from "./locations";
+// import river_locations from "./locations";
 import { useState, useEffect } from "react";
+// import { version } from "../package.json";
 
 // some constant definitions
-var fetch_url =
-  "https://api.existenz.ch/apiv1/hydro/latest?locations=2018&parameters=flow%2C%20temperature&app=MagicSwissWeed&version=0.1.0";
-// var fetch_url = "https://api.existenz.ch/apiv1/hydro/latest?";
+const river_locations = [
+  {
+    name: "Bremgarten",
+    id: "2018",
+    min: 180,
+    optimum: [230, 350],
+    max: 400,
+    danger: 480,
+  },
+  {
+    name: "Thun",
+    id: "2030",
+    min: 90,
+    optimum: [110, 190],
+    max: 210,
+    danger: 350,
+  },
+];
 
-// solve this with the map function
-// data.map(({ id, title }) => (
-//   <li key={id}>
-//     <h3>{title}</h3>
-//   </li>
-// ))}
-// fetch_url += "locations=" + locations.map();
+const bungee_locations = [
+  {
+    name: "Bern",
+    id: "2135",
+    min: 80,
+    optimum: 100,
+    danger: 360,
+  },
+  {
+    name: "Zürich",
+    id: "2099",
+    min: 75,
+    optimum: 95,
+    danger: 350,
+  },
+];
+
+const parameters = ["flow", "temperature"];
+
+// var fetch_url =
+// "https://api.existenz.ch/apiv1/hydro/latest?locations=2018&parameters=flow%2C%20temperature&app=MagicSwissWeed&version=0.1.0";
+var fetch_url = "https://api.existenz.ch/apiv1/hydro/latest?";
+
+// add variables to fetch_url
+fetch_url +=
+  "locations=" +
+  river_locations
+    .concat(bungee_locations)
+    .map(({ id }) => id)
+    .join("%2C");
+fetch_url += "&parameters=" + parameters.join("%2C");
+// TODO: get version from package.json
+fetch_url += "&app=MagicSwissWeed&version=0.1.0";
+
+function check_if_meas_undefined(meas) {
+  if (meas === undefined) {
+    meas = "N/A";
+  } else {
+    meas = meas.val;
+  }
+  return meas;
+}
+
+// render fetched data useful for displaying
+/**
+ *
+ * @param {Array} _data: length N * M
+ * @param {Array} _locations: length N
+ * @param {Array} _parameters: length M
+ * @returns
+ */
+function render_conditions(_data, _locations, _parameters) {
+  const N = _locations.length;
+  // const M = _parameters.length;
+
+  // combine _data and _locations into a single Array to render
+  let render_array = Array(N);
+  for (let i = 0; i < N; i++) {
+    // find the right measurements
+    let flow_i = _data.find(
+      ({ loc, par }) => loc === _locations[i].id && par === "flow"
+    );
+    flow_i = check_if_meas_undefined(flow_i);
+    let temp_i = _data.find(
+      ({ loc, par }) => loc === _locations[i].id && par === "temperature"
+    );
+    temp_i = check_if_meas_undefined(temp_i);
+
+    // then put them into a new container with all info for rendering
+    let render_dict = {
+      name: _locations[i].name,
+      flow: flow_i,
+      temp: temp_i,
+    };
+    render_array[i] = render_dict;
+    // TODO: handle undefined values
+  }
+  return (
+    <ul>
+      {render_array.map(({ name, flow, temp }) => (
+        // TODO: what is key used for?
+        <li key={name}>
+          <b>{name}</b>: {flow} m<sup>3</sup>/s, {temp}°C
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 // main React app that gets displayed
 export default function App() {
+  // if (ONLINE) {
   // define states for fetching data
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   // fetch data asynchronously and handle potential errors
   useEffect(() => {
     const getData = async () => {
@@ -47,12 +146,6 @@ export default function App() {
     getData();
   }, []);
 
-  // render fetched data useful for displaying
-  if (data) {
-    var flow = data.payload[0].val;
-    var temp = data.payload[1].val;
-  }
-
   // return HTML including variables computed above for displaying
   return (
     <div className="App">
@@ -65,11 +158,16 @@ export default function App() {
           <div>{`There is a problem fetching the post data - ${error}`}</div>
         )}
         {data && (
-          <ul>
-            <li>
-              Bremgarten: 🌊 {flow} m<sup>3</sup>/s, 🌡 {temp}°C
-            </li>
-          </ul>
+          <div className="surfspots">
+            <div className="riversurf">
+              <h2>Riversurf</h2>
+              {render_conditions(data.payload, river_locations, parameters)}
+            </div>
+            <div className="bungeesurf">
+              <h2>Bungeesurf</h2>
+              {render_conditions(data.payload, bungee_locations, parameters)}
+            </div>
+          </div>
         )}
       </header>
 
@@ -77,22 +175,25 @@ export default function App() {
       <footer>
         <ul className="Footer">
           <li className="Footer_item">
-            © 2023{" "}
-            <a className="Link" href="https://github.com/nkueng">
-              Nicola Küng
-            </a>{" "}
-            inspired by <a href="https://aare.guru">aare.guru</a>
-          </li>
-          <li className="Footer_item">
-            Hosted on{" "}
-            <a className="Link" href="https://pages.github.com">
-              GitHub Pages
+            <a
+              className="Link"
+              href="https://github.com/nkueng/MagicSwissWeed/issues"
+            >
+              Feedback
             </a>
           </li>
           <li className="Footer_item">
-            Data provided by{" "}
+            © 2023{" "}
+            {/* <a className="Link" href="https://academicsurfclub.ch"> */}
+            Academic Surf Club Switzerland
+            {/* </a> */}
+            <br />
+            inspired by <a href="https://aare.guru">aare.guru</a>
+          </li>
+          <li className="Footer_item">
+            Source:{" "}
             <a className="Link" href="https://www.hydrodaten.admin.ch">
-              BAFU/FOEN
+              BAFU
             </a>
           </li>
         </ul>
